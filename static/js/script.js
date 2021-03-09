dad4c5cd4b4b8d140afdbc4d5e0e69e7c881bcab
mcranter <mcranter@gmail.com>
2021-03-09 16:11:13 +0000

    form js added

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+//initialises text box resizing, add_word.html)
+$('#textarea1').val('New Text');
+M.textareaAutoResize($('#textarea1','#textarea2'));

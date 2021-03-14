f6918e582f184707974ca125c826d71c0705f3f7
mcranter <mcranter@gmail.com>
2021-03-14 16:32:44 +0000

    updated target ids

--- static/js/script.js
+++ static/js/script.js
@@ -3,18 +3,6 @@ $(document).ready(function(){
   });
 
 //initialises text box resizing, add_word.html)
-$('#textarea1').val('New Text');
-    M.textareaAutoResize($('#textarea1','#textarea2'));
-
-//pop up to confirm delete
-// var delete_word;
-// function sure() {
-//     if (confirm("Sure you want to delete this word?")) {
-
-//     } else {
-//         delete_word = "You pressed Cancel!";
-//     }
-//     document.getElementById("del_wrd").innerHTML = delete_word;
-//     }
-// sure()
+$('#task_name').val('');
+    M.textareaAutoResize($('#','#task_description'));
 
